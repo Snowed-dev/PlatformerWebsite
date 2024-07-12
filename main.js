7b0014f457d362e60c7aa5f2761d6773bf318032
RoshanDileep <118088610+RoshanDileep@users.noreply.github.com>
2024-07-12 14:36:25 -0700

    FInish animation
    Added  a spin animation to the button when its clicked, other things will be added in the future. (The button will expand to a bigger menu)

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 var d = new Date(); // for now9
 var minutes = d.getMinutes(); // =>  30
 var hours = d.getHours();
-var seconds
+var seconds; // Declare seconds
 var header = document.getElementById("time");
 
+hours -= 12;
 
-hours -=12
-
-if (minutes < 10){
-    minutes = String(minutes)
-    minutes = "0"+minutes
-
-
+if (minutes < 10) {
+    minutes = String(minutes);
+    minutes = "0" + minutes;
 }
 
-const time = ["",":",""]
-time[0] = hours
-time[2] = minutes
+const time = ["", ":", ""];
+time[0] = hours;
+time[2] = minutes;
 
-
-
-function updateTime(){
+function updateTime() {
     let timeString = time.join("");
-    header.innerHTML=timeString;
+    header.innerHTML = timeString;
 }
-updateTime()
+updateTime();
+
+function expandBtn() {
+    let expandible_button = document.getElementById("expandibleButton");
+
+    
+    expandible_button.style.animation = 'none';
+
+
+    expandible_button.offsetWidth; // Trigger reflow
+
+    
+    expandible_button.style.animation = 'spinButton 1s ease-in-out';
+}
